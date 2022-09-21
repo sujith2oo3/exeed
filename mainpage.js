5b7aad581d459ae0232ea4fdfd4d515133cd4731
sujith2oo3 <113540023+sujith2oo3@users.noreply.github.com>
2022-09-21 21:49:16 +0530

    Add files via upload

---
+++ mainpage.js
@@ -0,0 +1,44 @@
+document.getElementById("login").addEventListener("click",function(){
+    document.querySelector(".popup").style.display ="flex"
+})
+document.querySelector(".cancel").addEventListener("click",function(){
+    document.querySelector(".popup").style.display="none"
+})
+document.getElementById("user-main").addEventListener("click",function(){
+    document.querySelector(".user-popup").style.display ="flex"
+})
+document.querySelector(".cross").addEventListener("click",function(){
+    document.querySelector(".user-popup").style.display="none"
+})
+document.getElementById("worker-main").addEventListener("click",function(){
+    document.querySelector(".worker-popup").style.display ="flex"
+})
+document.querySelector(".cross2").addEventListener("click",function(){
+    document.querySelector(".worker-popup").style.display="none"
+})
+var enter = document.querySelector("#name1");
+
+enter.addEventListener("keypress",function (e){
+    document.querySelector("#email1").style.display="block"
+})
+document.querySelector("#email1").addEventListener("keypress",function(d){
+    document.querySelector("#password1").style.display="block"
+})
+document.querySelector("#password1").addEventListener("keypress",function(a){
+    document.querySelector("#passwordcheck1").style.display="block"
+})
+document.querySelector("#passwordcheck1").addEventListener("keypress",function(b){
+    document.querySelector("#date1").style.display="block"
+})
+document.querySelector("#date1").addEventListener("click",function(c){
+    document.querySelector("#Gender1").style.display="block"
+})
+document.querySelector("#Gender1").addEventListener("click",function(f){
+    document.querySelector("#address1").style.display="block"
+})
+document.querySelector("#address1").addEventListener("keypress",function(g){
+    document.querySelector("#checkbox1").style.display="block"
+    document.querySelector("#user-accept").style.display="block"    
+    document.querySelector("#user-login").style.display="block"    
+
+})
